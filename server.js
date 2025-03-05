ebc72c36b05d410c72a6025e5dff6c4522620608
solomonsitotaw <solomonsitotaw89@gmail.com>
2025-03-05 11:07:14 +0300

    slight update

--- server.js
+++ server.js
@@ -9,12 +9,26 @@ const express = require("express");
 const app = express();
 
 app.use(cookieParser());
+// app.use(
+//   cors({
+//     origin: "http://localhost:5173",
+//     credentials: true,
+//   })
+// );
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: (origin, callback) => {
+      const allowedOrigins = ["http://localhost:5173", "https://tick-sys.vercel.app/"];
+      if (allowedOrigins.includes(origin) || !origin) {
+        callback(null, true); // Allow the origin
+      } else {
+        callback(new Error("Not allowed by CORS")); // Deny the origin
+      }
+    },
     credentials: true,
   })
 );
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/user", userRoute);
